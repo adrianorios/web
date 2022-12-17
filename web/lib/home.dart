525258bb42f7cef200552f749be5b326e8c76c94
Adriano Rios <adrianorios@hotmail.com.br>
2022-12-17 13:48:20 -0400

    update screen

--- web/lib/home.dart
+++ web/lib/home.dart
@@ -18,10 +18,10 @@ class _HomeState extends State<Home> {
     String url =
         "https://buscapreco.sefaz.am.gov.br/item/grupo/page/1?termoCdGtin=&descricaoProd=${_produtoController.text}&action=&consultaExata=true&_consultaExata=on";
 
-    http.Response response = await http.post(
-      Uri.parse(url),
-    );
+    http.Response response = await http.post(Uri.parse(url));
+
     var document = parse(response.body);
+
     var cards = document.getElementsByClassName('card');
     for (var card in cards) {
       var cardTitle = card.getElementsByClassName('card-title');
@@ -49,98 +49,109 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.info_rounded,
-                color: Color(0xFF4CAF50),
-              )),
-        ],
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/logo.png",
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 15, right: 15, left: 15),
-                child: Image.asset(
-                  "assets/economizar.png",
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20, right: 25, left: 25),
-                child: TextFormField(
-                  controller: _produtoController,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey.withAlpha(50),
-                    isDense: true,
-                    hintText: "Digite o produto que deseja procurar",
-                    hintStyle:
-                        TextStyle(color: Colors.grey.shade400, fontSize: 14),
-                    prefixIcon: const Icon(
-                      Icons.search,
-                      color: Colors.black,
-                      size: 21,
+          child: GestureDetector(
+            onTap: () {
+              FocusManager.instance.primaryFocus?.unfocus();
+            },
+            child: SingleChildScrollView(
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.9,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  children: [
+                    Image.asset(
+                      "assets/logo.png",
+                      height: 200,
                     ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(60),
-                      borderSide: const BorderSide(
-                        width: 0,
-                        style: BorderStyle.none,
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 15, right: 15, left: 15),
+                      child: Image.asset(
+                        "assets/economizar.png",
+                        height: 30,
                       ),
                     ),
-                  ),
-                ),
-              ),
-              const Padding(padding: EdgeInsets.only(top: 20)),
-              SizedBox(
-                height: 50,
-                width: 200,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF4CAF50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18),
-                    ),
-                  ),
-                  onPressed: () {
-                    _recuperarCasos();
-                    _limpar();
-                  },
-                  child: const Text(
-                    "Pesquisar",
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: _produtos.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      onTap: () {
-                        // print("Item $_item clicado");
-                      },
-                      trailing: Text(_produtos[index]['valor'].trim()),
-                      title: Text(
-                        _produtos[index]['item'].trim(),
-                        style: const TextStyle(
-                            fontSize: 10, fontWeight: FontWeight.bold),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 20, right: 25, left: 25),
+                      child: TextFormField(
+                        controller: _produtoController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.grey.withAlpha(50),
+                          isDense: true,
+                          hintText: "Digite o produto que deseja procurar",
+                          hintStyle: TextStyle(
+                              color: Colors.grey.shade200, fontSize: 14),
+                          prefixIcon: const Icon(
+                            Icons.search,
+                            color: Colors.black,
+                            size: 21,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(60),
+                            borderSide: const BorderSide(
+                              width: 0,
+                              style: BorderStyle.none,
+                            ),
+                          ),
+                        ),
                       ),
-                    );
-                  },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: SizedBox(
+                        height: 50,
+                        width: 150,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFF4CAF50),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18),
+                            ),
+                          ),
+                          onPressed: () {
+                            _recuperarCasos();
+                            _limpar();
+                          },
+                          child: const Text(
+                            "Pesquisar",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: _produtos.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            color: const Color(0xFF4CAF50).withAlpha(150),
+                            child: ListTile(
+                              onTap: () {},
+                              trailing: Text(
+                                _produtos[index]['valor'].trim(),
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              title: Text(
+                                _produtos[index]['item'].trim(),
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  ],
                 ),
-              )
-            ],
+              ),
+            ),
           ),
         ),
       ),
